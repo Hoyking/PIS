c3566fcfad532720bcf2b6b7b767d784e94e511f
arpa0118 <parfenens@gmail.com>
2018-07-18 20:31:17 +0300

    Subtask PIS-19. Case 2

--- src/test/java/com/dreamteamrss/pis/story1/task1/Story1Task1Test.java
+++ src/test/java/com/dreamteamrss/pis/story1/task1/Story1Task1Test.java
@@ -18,40 +18,40 @@ public class Story1Task1Test {
         numberTheory = new NumberTheoryImpl();
     }
 
-    /*@Test
+    @Test
     public void sortTest() {
         int[] inputSequence = {0, -5, 10, -7, 6};
         int[] outputSequence = numberTheory.sort(inputSequence);
         assertTrue(Arrays.equals(outputSequence, new int[]{10, -7, 6, -5, 0}));
-    }*/
+    }
 
 
-    /*@Test
+    @Test
     public void diverseSequenceMethod1Test() {
         for (int index = 0; index < 1000000; index++) {
             List<Integer> inputSequence = Arrays.asList(13584, 657498, 1357498564, 486512348, -789, 24783365, 798456123);
             List<Integer> outputSequence = numberTheory.createDiverseSequenceMethod1(inputSequence);
             assertEquals(Arrays.asList(13584, 657498, -789, 798456123), outputSequence);
         }
-    }*/
+    }
 
-    /*@Test
+    @Test
     public void diverseSequenceMethod2Test() {
         for (int index = 0; index < 1000000; index++) {
             List<Integer> inputSequence = Arrays.asList(13584, 657498, 1357498564, 486512348, -789, 24783365, 798456123);
             List<Integer> outputSequence = numberTheory.createDiverseSequenceMethod2(inputSequence);
             assertEquals(Arrays.asList(13584, 657498, -789, 798456123), outputSequence);
         }
-    }*/
+    }
 
-    /*@Test
+    @Test
     public void diverseSequenceMethod3Test() {
         for (int index = 0; index < 1000000; index++) {
             List<Integer> inputSequence = Arrays.asList(13584, 657498, 1357498564, 486512348, -789, 24783365, 798456123);
             List<Integer> outputSequence = numberTheory.createDiverseSequenceMethod3(inputSequence);
             assertEquals(Arrays.asList(13584, 657498, -789, 798456123), outputSequence);
         }
-    }*/
+    }
 
     @Test
     public void diverseSequenceMethod4Test() {
